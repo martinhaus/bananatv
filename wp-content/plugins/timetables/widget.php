07a84904d66cce680c404345ec26708eb12d81c8
Martin <hauskrechtmartin@gmail.com>
2018-05-25 17:14:36 +0200

    Enh/Change timetable layout, add option for 3 timetables

--- wp-content/plugins/timetables/widget.php
+++ wp-content/plugins/timetables/widget.php
@@ -33,7 +33,9 @@ class timetable_widget extends WP_Widget {
 
 
 		//Room id
-		$room         = $instance['rooms'];
+		$rooms[]         = $instance['room1'];
+		$rooms[]         = $instance['room2'];
+		$rooms[]         = $instance['room3'];
 
 		global $wpdb;
 
@@ -43,89 +45,92 @@ class timetable_widget extends WP_Widget {
 		<div class="timetable-main">
 			<div id="room_name">
 			<?php
+			echo "</div>";
+		foreach ($rooms as $room) {
+			
+			$sql = "SELECT room from wp_timetables_timetables WHERE id = $room";
+			$room_name = $wpdb->get_results($sql);
+			
+			$sql = "SELECT name,teacher,date_format(start_time,'%H:%i') as start_time,date_format(end_time,'%H:%i') as end_time,TIME_TO_SEC(end_diff) as end_diff  FROM(
+					SELECT name,start_time, teacher, end_time,
+					CONCAT(timediff(time('2018-05-22 10:00:00.00'),
+						time(start_time))) as start_diff,
+					CONCAT(timediff(time('2018-05-22 10:00:00.00'),
+									time(end_time))) as end_diff,
+					day_of_week,timetable_id
+					FROM wp_timetables_lessons) as a
+					WHERE day_of_week = WEEKDAY('2018-05-22 10:00:00.00') and timetable_id = $room and start_diff > '0' and end_diff < '0'
+					ORDER BY start_diff DESC
+					LIMIT 1;";
+			$pending = $wpdb->get_results($sql);
+			echo "<div class='lessons'>";
+			echo "<div class='timetable-info room-name'>" . $room_name[0]->room . "</div>";
+			echo "<div id='current_lesson' class='lesson'>";
+			$time_till_end ="";
+			foreach ( $pending as $key => $row ) {
+				echo "<span class='acronym'>" . $this->timetables_subject_acronym($row->name)  . "</span><br>";
+				// echo "<span class='timetable-info'>" . $row->name . "</span><br>";
+				echo "<span class='timetable-info'>" . $row->teacher . "</span><br>";
+				echo "<span class='timetable-info'>" .$row->start_time . " - " . $row->end_time . "</span><br>";
+				$time_till_end = $row->end_diff;
+			}
 
-			// $sql = "SELECT room from wp_timetables_timetables WHERE id = $room";
-			// $room_name = $wpdb->get_results($sql);
-			// echo "<span class='timetable-info'> Miestnosť: " . $room_name[0]->room . "</span>";
+			if(!$pending) {
+				echo "";
+			} else {
+
+				$id = chr( rand( 65, 90 ) ) . chr( rand( 65, 90 ) ) . chr( rand( 65, 90 ) );
+				?>
+				<!-- Do konca zostáva  <span id="<?php echo $id ?>" class="timetable-highlight">00:00:00</span><br> -->
+				<span id="<?php echo $id ?>" class="timetable-highlight">00:00:00</span><br>
+				<script
+					type="text/javascript">startTimer(<?php echo abs( $time_till_end ) ?>, document.querySelector('#<?php echo $id ?>'));</script>
+				<?php
+			}
 			echo "</div>";
 
 
-		$sql = "SELECT name,teacher,date_format(start_time,'%H:%i') as start_time,date_format(end_time,'%H:%i') as end_time,TIME_TO_SEC(end_diff) as end_diff  FROM(
-				SELECT name,start_time, teacher, end_time,
-				  CONCAT(timediff(time('2018-05-22 10:00:00.00'),
-				      time(start_time))) as start_diff,
-				  CONCAT(timediff(time('2018-05-22 10:00:00.00'),
-				                  time(end_time))) as end_diff,
-				  day_of_week,timetable_id
-				FROM wp_timetables_lessons) as a
-				WHERE day_of_week = WEEKDAY('2018-05-22 10:00:00.00') and timetable_id = $room and start_diff > '0' and end_diff < '0'
-				ORDER BY start_diff DESC
-				LIMIT 1;";
-		$pending = $wpdb->get_results($sql);
-		echo "<div class='lessons'>";
+			echo "<div id='next_lesson' class='lesson'>";
+			$sql = "SELECT name,teacher,DATE_FORMAT(start_time,'%H:%i') as start_time,
+					TIME_TO_SEC(diff) as diff, DATE_FORMAT(end_time,'%H:%i') as end_time FROM(
+					SELECT name,start_time, end_time, teacher, CONCAT(timediff(time('2018-05-22 10:00:00.00'),
+					time(start_time))) as diff,day_of_week,timetable_id
+					FROM wp_timetables_lessons) as a
+					WHERE day_of_week = WEEKDAY('2018-05-22 10:00:00.00') and timetable_id = $room and diff < '0'
+					ORDER BY diff DESC
+					LIMIT 1;
+					";
 
-		echo "<div id='current_lesson' class='lesson'>";
-		$time_till_end ="";
-		foreach ( $pending as $key => $row ) {
-			echo "<span class='acronym'>" . $this->timetables_subject_acronym($row->name)  . "</span><br>";
-			// echo "<span class='timetable-info'>" . $row->name . "</span><br>";
-			echo "<span class='timetable-info'>" . $row->teacher . "</span><br>";
-			echo "<span class='timetable-info'>" .$row->start_time . " - " . $row->end_time . "</span><br>";
-			$time_till_end = $row->end_diff;
+
+			$next_hour = $wpdb->get_results($sql);
+			$time_till_end ="";
+			foreach ($next_hour as $key => $row) {
+				echo "<span class='acronym'>" . $this->timetables_subject_acronym($row->name)  . "</span><br>";
+				// echo "<span class='timetable-info'>" . $row->name . "</span><br>";
+				echo "<span class='timetable-info'>" . $row->teacher . "</span><br>";
+				echo "<span class='timetable-info'>" . $row->start_time . " - " . $row->end_time . "</span><br>";
+
+				$time_till_end = $row->diff;
+			}
+
+			if(!$next_hour) {
+				echo "Nasleduje: - <br>";
+			} else {
+
+				//Print countdown timer
+				$id = chr( rand( 65, 90 ) ) . chr( rand( 65, 90 ) ) . chr( rand( 65, 90 ) );
+				?>
+				<!-- Do začiatku zostáva  <span id="<?php echo $id ?>" class="timetable-highlight">00:00:00</span> -->
+				<span id="<?php echo $id ?>" class="timetable-highlight">00:00:00</span>
+				<script
+					type="text/javascript">startTimer(<?php echo abs( $time_till_end ) ?>, document.querySelector('#<?php echo $id ?>'));</script>
+				<?php
+
+			}
+			echo "</div>";
+			echo "</div>";
 		}
-
-		if(!$pending) {
-			echo "Prebieha: - <br>";
-		} else {
-
-			$id = chr( rand( 65, 90 ) ) . chr( rand( 65, 90 ) ) . chr( rand( 65, 90 ) );
-			?>
-			Do konca zostáva  <span id="<?php echo $id ?>" class="timetable-highlight">00:00:00</span><br>
-			<script
-				type="text/javascript">startTimer(<?php echo abs( $time_till_end ) ?>, document.querySelector('#<?php echo $id ?>'));</script>
-			<?php
-		}
-		echo "</div>";
-
-
-		echo "<div id='next_lesson' class='lesson'>";
-		$sql = "SELECT name,teacher,DATE_FORMAT(start_time,'%H:%i') as start_time,
-				TIME_TO_SEC(diff) as diff, DATE_FORMAT(end_time,'%H:%i') as end_time FROM(
-				SELECT name,start_time, end_time, teacher, CONCAT(timediff(time('2018-05-22 10:00:00.00'),
-				time(start_time))) as diff,day_of_week,timetable_id
-				FROM wp_timetables_lessons) as a
-				WHERE day_of_week = WEEKDAY('2018-05-22 10:00:00.00') and timetable_id = $room and diff < '0'
-				ORDER BY diff DESC
-				LIMIT 1;
-				";
-
-
-		$next_hour = $wpdb->get_results($sql);
-		$time_till_end ="";
-		foreach ($next_hour as $key => $row) {
-			echo "<span class='acronym'>" . $this->timetables_subject_acronym($row->name)  . "</span><br>";
-			// echo "<span class='timetable-info'>" . $row->name . "</span><br>";
-			echo "<span class='timetable-info'>" . $row->teacher . "</span><br>";
-			echo "<span class='timetable-info'>" . $row->start_time . " - " . $row->end_time . "</span><br>";
-
-			$time_till_end = $row->diff;
-		}
-
-		if(!$next_hour) {
-			echo "Nasleduje: - <br>";
-		} else {
-
-			//Print countdown timer
-			$id = chr( rand( 65, 90 ) ) . chr( rand( 65, 90 ) ) . chr( rand( 65, 90 ) );
-			?>
-			Do začiatku zostáva  <span id="<?php echo $id ?>" class="timetable-highlight">00:00:00</span>
-			<script
-				type="text/javascript">startTimer(<?php echo abs( $time_till_end ) ?>, document.querySelector('#<?php echo $id ?>'));</script>
-			<?php
-
-		}
-		echo "</div>";
-		echo "</div>";
+		
 		?>
 		</div>
 		<?php
@@ -136,7 +141,6 @@ class timetable_widget extends WP_Widget {
         <script type="text/javascript">
         jQuery(document).ready(function () {
             jQuery('.content-area').css('width','100%');
-            console.log('aaa');
         });
         </script>
         
@@ -152,25 +156,50 @@ class timetable_widget extends WP_Widget {
 		// outputs the options form on admin
 
 		$instance = wp_parse_args( (array) $instance, array(
-			'rooms' => 'lala',
+			'room1' => '',
+			'room2' => '',
+			'room3' => '',
 		));
-		$rooms = $instance['rooms'];
+		$room1 = $instance['room1'];
+		$room2 = $instance['room2'];
+		$room3 = $instance['room3'];
 
 		global $wpdb;
 		$sql = "SELECT id,room FROM wp_timetables_timetables";
 		$options = $wpdb->get_results($sql);
 		?>
 
-		<select id = <?php echo $this->get_field_id('rooms'); ?> name=<?php echo $this->get_field_name('rooms'); ?> class="widefat">
+		<select id = <?php echo $this->get_field_id('room1'); ?> name=<?php echo $this->get_field_name('room1'); ?> class="widefat">
 			<?php
 			foreach ( $options as $key => $row ) {
-				$selected = ( $rooms == $row->id ) ?
+				$selected = ( $room1 == $row->id ) ?
 					'selected="selected"' : '';
 				echo '<option value =' . "\"$row->id\" " . $selected . '>' . $row->room . '</option>';
 			}
 			?>
 		</select>
 
+		<select id = <?php echo $this->get_field_id('room2'); ?> name=<?php echo $this->get_field_name('room2'); ?> class="widefat">
+			<option value="">--</option>
+			<?php
+			foreach ( $options as $key => $row ) {
+				$selected = ( $room2 == $row->id ) ?
+					'selected="selected"' : '';
+				echo '<option value =' . "\"$row->id\" " . $selected . '>' . $row->room . '</option>';
+			}
+			?>
+		</select>
+
+		<select id = <?php echo $this->get_field_id('rooms'); ?> name=<?php echo $this->get_field_name('room3'); ?> class="widefat">
+			<option value=""></option>
+			<?php
+			foreach ( $options as $key => $row ) {
+				$selected = ( $room3 == $row->id ) ?
+					'selected="selected"' : '';
+				echo '<option value =' . "\"$row->id\" " . $selected . '>' . $row->room . '</option>';
+			}
+			?>
+		</select>
 		<?php
 	}
 
@@ -184,7 +213,9 @@ class timetable_widget extends WP_Widget {
 
 		$instance = $old_instance;
 
-		$instance['rooms'] = $new_instance['rooms'];
+		$instance['room1'] = $new_instance['room1'];
+		$instance['room2'] = $new_instance['room2'];
+		$instance['room3'] = $new_instance['room3'];
 
 		return $instance;
 	}
