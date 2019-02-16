2bce11a7e14dd6c10bb8eb63eaf8dfe1d52890c3
Martin <hauskrechtmartin@gmail.com>
2019-02-16 12:14:04 +0100

    Add no translate header

--- wp-content/themes/fiit/header.php
+++ wp-content/themes/fiit/header.php
@@ -13,6 +13,7 @@
 <html <?php language_attributes(); ?> class="no-js">
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="google" content="notranslate">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
